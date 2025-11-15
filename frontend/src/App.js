5882617c0cdbbf35d6dd116bc1c17a52a6ff568a
Mukund Deotale <mukundeotale9860@gmail.com>
2025-11-15 18:36:38 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Frontend Running (React)</h1>
+      <p>This is the UI talking to Backend API</p>
+    </div>
+  );
+}
+
+export default App;
